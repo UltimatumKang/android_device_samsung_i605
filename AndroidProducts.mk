22c6e1b5ab4caa22ba9183be56ea074728ea6b54
David Hou <david.hou314@gmail.com>
2013-05-23 18:30:39 -0400

    Proper UKG Rebase

--- AndroidProducts.mk
+++
@@ -1,1 +0,0 @@
-PRODUCT_MAKEFILES += device/samsung/i605/full_i605.mk
--- ukg.mk
+++ ukg.mk
@@ -1,31 +1,52 @@
-# Inherit AOSP device configuration for t0lte.
-$(call inherit-product, device/samsung/i605/full_i605.mk)
+#
+# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013 The Xylon Experimental
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit common product files.
-$(call inherit-product, vendor/ukg/config/common.mk)
+# name
+PRODUCT_RELEASE_NAME := GN
 
-# t0lte Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/t0lte
+# device
+$(call inherit-product-if-exists, device/samsung/i605/full_i605.mk)
 
-# Inherit GSM common stuff
-$(call inherit-product, vendor/ukg/config/cdma.mk)
+# gsm
+$(call inherit-product, vendor/ukg/config/common_gsm.mk)
 
-# Tablet Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/ukg/overlay/common
+# languages
+PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
-# Setup device specific product configuration.
-PRODUCT_NAME := ukg_i605
-PRODUCT_BRAND := samsung
+# phone
+$(call inherit-product, vendor/ukg/config/common_phone.mk)
+
+# products
 PRODUCT_DEVICE := i605
-PRODUCT_MODEL := SCH-I605
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := Samsung
+PRODUCT_NAME := ukg_i605
+PRODUCT_MODEL := Samsung Galaxy Note 2
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=ukg.i605.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.1.1/JRO03C/I605VRALL4:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.1.1 JRO03C I605VRALL4 release-keys"
+# overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_NUMBER=573038 \
+    PRODUCT_NAME=i605 \
+    TARGET_BUILD_TYPE=user \
+    BUILD_VERSION_TAGS=release-keys \
+    PRIVATE_BUILD_DESC="i605-user 4.2.2 JDQ39 573038 release-keys" \
+    BUILD_FINGERPRINT="samsung/i605:4.2.2/JDQ39/573038:user/release-keys"
 
-# Boot Animation
+# hybrid
 PRODUCT_COPY_FILES +=  \
-    vendor/ukg/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf \
-    vendor/ukg/prebuilt/bootanimation/bootanimation_1280_720.zip:system/media/bootanimation.zip \
-    vendor/ukg/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/ukg/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
-
+    vendor/ukg/prebuilt/hybrid_xhdpi_no_nav.conf:system/etc/beerbong/properties.conf
